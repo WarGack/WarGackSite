d241046687a43affe16c6dfc551d2cc6b43de551
WarGack <53942687+WarGack@users.noreply.github.com>
2025-12-05 02:54:57 +0300

    Remove callback handling from fetch function
    Removed callback handling from fetch method.

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
 export default {
   async fetch(req) {
     const url = new URL(req.url);
-    if (url.pathname === "/callback") {
-      if (req.method !== "POST") return new Response("Method not allowed", { status: 405 });
-      // Для теста просто отвечаем YES
-      return new Response("YES", { status: 200, headers: { "Content-Type": "text/plain" } });
-    }
     return new Response("OK");
   }
 }
