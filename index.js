59c2a7c6b1336c6c1d81aed7715a96876eff41e7
WarGack <53942687+WarGack@users.noreply.github.com>
2025-12-05 02:58:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-export function onRequest(context) {
-  return new Response("Hello, world!")
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const data = await fetch("https://donatepay.ru/api/v1/transactions?access_token=GtbWY4qqxy5HUJWm761Mcq0lSwyBElC41ToR2AXUjSkJBed2idPPKbPIsSaW&status=success&limit=100&type=donation")
+  const facts = await data.json();
+  return new Response(JSON.stringify(facts), {status: 200})
 }
