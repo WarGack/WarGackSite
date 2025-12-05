c6229b3c5963eb841eb82bb3fbfa633f1219ce11
WarGack <53942687+WarGack@users.noreply.github.com>
2025-12-05 17:33:59 +0300

    Implement API call to fetch donation transactions
    Fetch donation data from external API and return as JSON response.

--- _worker.js
+++ _worker.js
@@ -3,7 +3,9 @@ export default {
     const url = new URL(request.url);
     if (url.pathname.startsWith("/api/")) {
       // TODO: Add your custom /api/* logic here.
-      return new Response("Ok");
+        const data = await fetch("https://donatepay.ru/api/v1/transactions?access_token=GtbWY4qqxy5HUJWm761Mcq0lSwyBElC41ToR2AXUjSkJBed2idPPKbPIsSaW&status=success&limit=100&type=donation")
+  const facts = await data.json();
+  return new Response(JSON.stringify(facts), {status: 200})
     }
     // Otherwise, serve the static assets.
     // Without this, the Worker will error and no assets will be served.
