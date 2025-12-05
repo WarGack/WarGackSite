7bdf6efba041cf74c018b4c995e31ce1077c070d
WarGack <53942687+WarGack@users.noreply.github.com>
2025-12-05 03:15:27 +0300

    Refactor fetch handling to use event listener

--- functions/test.js
+++ functions/test.js
@@ -1,13 +1,9 @@
-export default {
-  async fetch(req) {
-    const url = new URL(req.url);
-    if (url.pathname === '/callback') {
-      if (req.method !== 'POST') return new Response('Method not allowed', { status: 405 });
-      return new Response('YES', { headers: { 'Content-Type': 'text/plain' }});
-    }
-    if (url.pathname === '/success') {
-      return new Response('<html><body><h1>Оплата успешна</h1></body></html>', { headers: { 'Content-Type': 'text/html; charset=utf-8' }});
-    }
-    return new Response('OK');
-  }
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const data = await fetch("https://donatepay.ru/api/v1/transactions?access_token=GtbWY4qqxy5HUJWm761Mcq0lSwyBElC41ToR2AXUjSkJBed2idPPKbPIsSaW&status=success&limit=100&type=donation")
+  const facts = await data.json();
+  return new Response(JSON.stringify(facts), {status: 200})
 }
